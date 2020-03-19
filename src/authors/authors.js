93b54be44687d5c2bee3dcfa51ee3a8985e9676f
minzhenli <min.205@osu.edu>
2020-03-19 00:40:57 -0400

    commit

--- src/authors/authors.js
+++ src/authors/authors.js
@@ -1,39 +1,21 @@
-import React from "react";
-import "./authors.css";
-import { Component } from "react";
+import React, { useEffect, useState } from "react";
 import { AuthorDisplay } from "./AuthorDisplay";
 
-export default class authors extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            authors: [
-            {
-              img: "https://images-na.ssl-images-amazon.com/images/I/51TjKI58LiL._US230_.jpg",
-              name: "Eric Evans",
-              country: "USA"
-            },
-            {
-              img: "https://federalnewsnetwork.com/wp-content/uploads/2017/05/2017NicoleOgrysko-960x960-300x300.jpg",
-              name: "Nicole Forsgren",
-              country: "USA"
-            },
-            {
-              img: "https://www.biography.com/.image/ar_1:1%2Cc_fill%2Ccs_srgb%2Cg_face%2Cq_auto:good%2Cw_300/MTE5NTU2MzE2NDI3NDIxMTk1/tom-clancy-9542178-1-402.jpg",
-              name: "Tom Clancy",
-              country: "USA"
-            }
-            ]
-        }
-    }
+export default function Authors (props) {
+    const [data, setData] = useState([]);
 
-    render() {        
+    useEffect(() => {
+      fetch("http://localhost:5001/api/authors/")
+      .then(response => response.json())
+      .then(data => setData(data));
+    }, []);
+
     return (
-        <div className="authors">
-            <div className="lander">
-            <AuthorDisplay authors={this.state.authors} />
-            </div>
+      <div className="Authors">
+        <div className="lander">
+          <AuthorDisplay Authors={data} />
         </div>
+      </div>
     );
-    }
+    
 }
--- src/books/Books.js
+++ src/books/Books.js
@@ -1,79 +1,21 @@
-import React, { Component } from "react";
-import "./Books.css";
+import React, { useEffect, useState } from "react";
 import { BookDisplay } from "./BookDisplay";
 
-export default class Books extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-        books: [
-          {
-            id: 1,
-            title: "Domain Driven Design",
-            author: "Eric Evans",
-            isbn: "978-0321125217",
-            img: "https://prodimage.images-bn.com/pimages/9780321125217_p0_v2_s600x595.jpg"
-        },
-        {
-            id: 2,
-            title: "Accelerate",
-            author: "Nicole Forsgren",
-            isbn: "978-1942788331",
-            img: "https://prodimage.images-bn.com/pimages/9781942788331_p0_v2_s550x406.jpg"
-        },
-        {
-            id: 3,
-            title: "The Cardinal of the Kremlin",
-            author: "Tom Clancy",
-            isbn: "978-1101002384" ,
-            img: "https://prodimage.images-bn.com/pimages/9780425269398_p0_v2_s550x406.jpg"
-        },
-        {
-            id: 4,
-            title: "As a Man Thinketh",
-            author: "James Allen",
-            isbn: "978-0875160009",
-            img: "https://prodimage.images-bn.com/pimages/9781585426386_p0_v1_s550x406.jpg" 
-        },
-        {
-            id: 5,
-            title: "The Acts of the Apostles",
-            author: "Ellen G. White",
-            isbn: "978-1644391136", 
-            img: "https://prodimage.images-bn.com/pimages/9781514280966_p0_v1_s550x406.jpg"
-        },
-        {
-            id: 6,
-            title: "To Kill a Mockingbird",
-            author: "Harper Lee",
-            isbn: "978-0446310789", 
-            img: "https://prodimage.images-bn.com/pimages/9780446310789_p0_v6_s550x406.jpg"
-        },
-        {
-            id: 7,
-            title: "Tinkers",
-            author: "Paul Harding",
-            isbn: "978-1934137123", 
-            img: "https://prodimage.images-bn.com/pimages/9781934137123_p0_v3_s550x406.jpg"
-        },
-        {
-            id: 8,
-            title: "Cutting for Stone",
-            author: "Abraham Verghese",
-            isbn: "978-0375714368", 
-            img: "https://prodimage.images-bn.com/pimages/9780375714368_p0_v1_s550x406.jpg"
-        }
-      ]
-    };
-  }
+export default function Books(props) {
+    const [data, setData] = useState([]);
 
-  render() {
+    useEffect(() => {
+      fetch("http://localhost:5001/api/books/")
+      .then(response => response.json())
+      .then(data => setData(data));
+    }, []);
+
     return (
-        <div className="Books">
-            <div className="lander">
-              <BookDisplay books={this.state.books} />
-            </div>
+      <div className="Books">
+        <div className="lander">
+          <BookDisplay books={data} />
         </div>
+      </div>
     );
-  }
+    
 }
